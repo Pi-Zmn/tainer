56246bbc78199c43d371b6223fcd71869ce3cab3
Philipp <philipp.zimmermann@bevelop.de>
2023-12-28 17:27:01 +0100

    Create Empty React Project

---
+++ tainer-frontend/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Krasse Überschrift</h1>
+      <p>Krasser Inhalt</p>
+    </div>
+  )
+}
