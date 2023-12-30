f791e1d3beea81f420f54a812d64bc32ed27d5cd
Philipp <philipp.zimmermann@bevelop.de>
2023-12-30 22:15:13 +0100

    start knopf

--- tainer-frontend/src/app/page.tsx
+++ tainer-frontend/src/app/page.tsx
@@ -1,11 +1,27 @@
+'use client'
+
+import React, {useEffect, useState} from "react";
+import Button from "@mui/material/Button";
+
 import YouTubePlayer from "@/components/YouTubePlayer/YouTubePlayer";
+import styles from './page.module.css'
 
 export default function Home() {
     const videoID: string = '8TxSgMUMIP4';
+    const [showVideo, setShowVideo] = useState<boolean>(false);
+
+    const toggleShowVideo = () => {
+        setShowVideo(!showVideo);
+    }
+
   return (
-    <div>
-        <h1>Willkommen bei Kotzenplotz!</h1>
-      <YouTubePlayer videoId={videoID} />
-    </div>
+      showVideo ?
+          <div>
+              <h1>Willkommen bei Kotzenplotz!</h1>
+              <YouTubePlayer videoId={videoID} />
+          </div> :
+          <div className={styles.buttonContainer}>
+              <Button variant="outlined" onClick={toggleShowVideo}>Klick mich</Button>
+          </div>
   )
 }
