da1e9ec18d69fb61a30c698839e03858a97a89b7
lucas <wenzel-lucas@gmx.net>
2023-12-30 22:26:58 +0100

    adds story cards for story time page

--- tainer-frontend/src/app/Storytime/page.tsx
+++ tainer-frontend/src/app/Storytime/page.tsx
@@ -1,8 +1,20 @@
-export default function StoryTime() {
+import {Grid} from "@mui/material";
+import StoryCard from "@/components/StoryCard/StoryCard";
+import {stories} from "../../../public/staticFiles/stories";
+
+export default function StoryTime () {
     return (
         <div>
             <h1>Krasse Storys aus Robos Leben</h1>
-            <p>Krasser Storys</p>
+            <Grid container spacing={2}>
+                {stories.map((story, index) => {
+                    return (
+                        <Grid item xs={12} sm={6} key={`story-${index}`}>
+                            <StoryCard title={story.title} textContent={story.textContent} imgSrc={story.imgSrc}/>
+                        </Grid>
+                    )
+                })}
+            </Grid>
         </div>
     )
 }
