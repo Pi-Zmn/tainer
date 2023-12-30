d90cfdd280933f4ef34cb0bfce9c19f976d823a9
Philipp <philipp.zimmermann@bevelop.de>
2023-12-30 19:53:14 +0100

    calender

--- tainer-frontend/src/app/Calender/page.tsx
+++ tainer-frontend/src/app/Calender/page.tsx
@@ -1,8 +1,19 @@
+'use client'
+
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
+import styles from './page.module.scss';
+
 export default function Calender() {
     return (
         <div>
             <h1>Krasser Kalender von Robo</h1>
-            <p>Krasser Termine die hier gemacht werden</p>
+            <div className={styles.calenderContainer}>
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <DateCalendar />
+                </LocalizationProvider>
+            </div>
         </div>
     )
 }
